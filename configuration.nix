a744ed5e50c95d5b28b8a9c2cbecdeaaf5492632
rekkasa <alexandros.rekkas@gmail.com>
2021-09-21 22:13:55 +0200

    update default to zsh

--- configuration.nix
+++ configuration.nix
@@ -85,7 +85,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.arekkas = {
     isNormalUser = true;
-    shell = packages.zsh;
+    shell = pkgs.zsh;
     extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
   };
 
